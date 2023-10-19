db06c699612b4375c5162e72abc5589bd150ca48
adamtimm <amtimm@gmail.com>
2023-10-19 09:39:50 -0500

    admin cleanup

--- setup.sh
+++
@@ -1,7 +0,0 @@
-# ArgoCD needs to be installed before this file is run
-# must also be logged into the primary cluster prior to running this file
-# 1) create neccessary namespaces
-kubectl apply -k postgres-operator-examples/kustomize/install/namespace --validate=false
-
-# 2) 
-
